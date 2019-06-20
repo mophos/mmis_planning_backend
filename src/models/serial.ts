eed062755f716cb17228b0f8c549052ee433d572
naphattharawat <naphattharawat@gmail.com>
2019-06-20 15:03:44 +0700

    ลูกหนี้เจ้าหนี้

--- src/models/serial.ts
+++ src/models/serial.ts
@@ -2,68 +2,104 @@ import Knex = require('knex');
 import * as moment from 'moment';
 
 export class SerialModel {
-  getSerialInfo(knex: Knex, srType: string) {
+
+  getSerialInfo(knex: Knex, srType: string, year, warehouseId) {
     return knex('sys_serials as sr')
+      .select('sr.*', 'sf.*', 'w.short_code')
       .where('sr.sr_type', srType)
-      .select('sr.sr_no', 'sr.sr_year', 'sr.is_year_prefix', 'sr.sr_prefix', 'sr.digit_length', 'sf.serial_code')
+      .where('sr.sr_year', year)
+      .where('sr.warehouse_id', warehouseId)
+      .select('sr.sr_no', 'sr.sr_prefix', 'sr.digit_length', 'sf.serial_code')
       .leftJoin('sys_serial_format as sf', 'sf.serial_format_id', 'sr.serial_format_id')
+      .leftJoin('wm_warehouses as w', 'w.warehouse_id', 'sr.warehouse_id')
       .limit(1);
   }
 
-  async getSerial(knex: Knex, srType: string) {
+  getSerialDetail(knex: Knex, srType: string) {
+    return knex('sys_serials as sr')
+      .where('sr.sr_type', srType)
+      .limit(1);
+  }
 
-    let serialInfo = await this.getSerialInfo(knex, srType);
+  insertSerialInfo(knex: Knex, data) {
+    return knex('sys_serials')
+      .insert(data);
+  }
+
+  async getSerial(knex: Knex, srType: string, year, warehouseId) {
+    let serialInfo = await this.getSerialInfo(knex, srType, year, warehouseId);
+
     if (serialInfo.length) {
       let currentNo = serialInfo[0].sr_no;
       let serialCode = serialInfo[0].serial_code;
       let serialLength = serialInfo[0].digit_length;
       let serialPrefix = serialInfo[0].sr_prefix;
-
-      if (+serialInfo[0].sr_year !== moment().get('year')) {
-        // reset serial
-        await this.resetSerial(knex, srType);
-        currentNo = 1;
-      }
-
-      let serialYear = moment().get('year') + 543;
+      let warehouseCode = serialInfo[0].short_code;
+      let serialYear = +year + 543;
       let _serialYear = serialYear.toString().substring(2);
       let newSerialNo = this.paddingNumber(currentNo, serialLength);
+      let _warehouseNo = this.paddingNumber(warehouseId, 2);
 
       let sr: any = null;
+      sr = serialCode.replace('PREFIX', serialPrefix).replace('YY', _serialYear).replace('WW', _warehouseNo).replace('WC', warehouseCode).replace('##', newSerialNo);
 
-      if (serialInfo[0].is_year_prefix === 'Y') {
-        sr = serialCode.replace('PREFIX', serialPrefix).replace('YY', _serialYear).replace('##', newSerialNo);
-      } else {
-        sr = serialCode.replace('PREFIX', serialPrefix).replace('-YY', '').replace('##', newSerialNo);
-      }
 
       // update serial
-      await this.updateSerial(knex, srType);
-      // return serial
+      await this.updateSerial(knex, srType, year, warehouseId);
       return sr;
 
     } else {
-      return this.paddingNumber(0, serialInfo[0].digit_length || 6);
+      let serialDetail = await this.getSerialDetail(knex, srType);
+      const obj = {
+        sr_type: serialDetail[0].sr_type,
+        sr_prefix: serialDetail[0].sr_prefix,
+        sr_no: 1,
+        sr_year: year,
+        digit_length: serialDetail[0].digit_length,
+        serial_format_id: serialDetail[0].serial_format_id,
+        comment: serialDetail[0].comment,
+        warehouse_id: warehouseId
+      }
+      await this.insertSerialInfo(knex, obj);
+
+
+      let serialInfo = await this.getSerialInfo(knex, srType, year, warehouseId);
+      let currentNo = serialInfo[0].sr_no;
+      let serialCode = serialInfo[0].serial_code;
+      let serialLength = serialInfo[0].digit_length;
+      let serialPrefix = serialInfo[0].sr_prefix;
+      let warehouseCode = serialInfo[0].short_code;
+      let serialYear = +year + 543;
+      let _serialYear = serialYear.toString().substring(2);
+      let newSerialNo = this.paddingNumber(currentNo, serialLength);
+      let _warehouseNo = this.paddingNumber(warehouseId, 2);
+      let sr: any = null;
+
+      sr = serialCode.replace('PREFIX', serialPrefix).replace('YY', _serialYear).replace('WW', _warehouseNo).replace('WC', warehouseCode).replace('##', newSerialNo);
+
+
+      // update serial
+      await this.updateSerial(knex, srType, year, warehouseId);
+
+      // return serial
+      return sr;
     }
   }
 
-  paddingNumber(n: number, p: number) {
+  paddingNumber(currentNo: number, serialLength: number) {
+    if (currentNo.toString().length > serialLength) {
+      serialLength = currentNo.toString().length;
+    }
     var pad_char = '0';
-    var pad = new Array(1 + p).join(pad_char);
-    return (pad + n).slice(-pad.length);
+    var pad = new Array(1 + serialLength).join(pad_char);
+    return (pad + currentNo).slice(-pad.length);
   }
 
-  async updateSerial(knex: Knex, srType: string) {
+  async updateSerial(knex: Knex, srType: string, year, warehouseId) {
     return knex('sys_serials')
       .increment('sr_no', 1)
-      .where('sr_type', srType);
+      .where('sr_type', srType)
+      .where('sr_year', year)
+      .where('warehouse_id', warehouseId);
   }
-
-  async resetSerial(knex: Knex, srType: string) {
-    return knex('sys_serials')
-      .update('sr_no', 1)
-      .update('sr_year', moment().get('year'))
-      .where('sr_type', srType);
-  }
-
 }
